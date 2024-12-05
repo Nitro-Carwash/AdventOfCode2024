56f373901abef0c57817407d2950c86a021f46d9
Nitro Carwash <>
2024-12-05 11:27:14 -0800

    day 1

--- day01.py
+++ day01.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import re
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def load_input():
+    col1 = []
+    col2 = []
+    with open('in01', 'r') as in_stream:
+        for line in in_stream.readlines():
+            groups = re.match(r"(\d+)\s+(\d+)", line)
+            col1.append(int(groups.group(1)))
+            col2.append(int(groups.group(2)))
+
+    return col1, col2
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Part 1
+def calculate_total_distance(col1, col2):
+    col1 = sorted(col1)
+    col2 = sorted(col2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    total_distance = 0
+
+    for i in range(len(col1)):
+        total_distance += abs(col1[i] - col2[i])
+
+    return total_distance
+
+
+# Part 2
+def calculate_similarity_score(col1, col2):
+    freq_dict = {}
+    for v in col2:
+        if v in freq_dict:
+            freq_dict[v] += 1
+        else:
+            freq_dict[v] = 1
+
+    total_similarity = 0
+    for v in col1:
+        if v in freq_dict:
+            total_similarity += v * freq_dict[v]
+
+    return total_similarity
+
+
+col1, col2 = load_input()
+print(f"total distance: {calculate_total_distance(col1, col2)}")
+print(f"similarity score: {calculate_similarity_score(col1, col2)}")
